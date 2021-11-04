cbf545699d753a3a166ea65f310e8edbd71d1b98
aag031 <alexander.l.gorlov@gmail.com>
2021-11-04 21:07:09 +0300

    add temporary variant init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- neo vim configuration programm
+    -- neo vim configuration programm
 -- version A200
 --
 -- Aliases Definition Begin--
@@ -24,6 +24,17 @@ vim.g['airline#extensions#keymap#enabled'] = 0
 vim.g['airline_section_z'] = "%l/%L Col:%c"
 vim.g['Powerline_symbols']='unicode'
 vim.g['airline#extensions#xkblayout#enabled'] = 0
+vim.g.gutentags_trace = true
+vim.g.gutentags_add_default_project_roots = false
+vim.g.gutentags_project_root = {'package.json', '.git'}
+vim.g.gutentags_cache_dir = vim.fn.expand('~/.cache/nvim/ctags/')
+vim.g.gutentags_generate_on_new = true
+vim.g.gutentags_generate_on_missing = true
+vim.g.gutentags_generate_on_write = true
+vim.g.gutentags_generate_on_empty_buffer = true
+vim.cmd([[command! -nargs=0 GutentagsClearCache call system('rm ' . g:gutentags_cache_dir . '/*')]])
+vim.g.gutentags_ctags_extra_args = {'--tag-relative=yes', '--fields=+ailmnS', }
+
 
 -- Plugin imstallation section
 local install_path = fn.stdpath('data')..'/site/pack/paqs/opt/paq-nvim'
@@ -48,57 +59,17 @@ plug {'mhinz/vim-signify', opt=false}
 plug {'tpope/vim-fugitive', opt=false}
 plug {'junegunn/gv.vim', opt=false}
 plug {'neovim/nvim-lspconfig', opt=false}
-plug {'nvim-lua/completion-nvim', opt=false}
--- Setup LSP --
-local nvim_lsp = require('lspconfig')
+plug {'neoclide/coc.nvim', opt=false}
+plug {'ludovicchabant/vim-gutentags', opt=false}
+plug {'majutsushi/tagbar', opt=false}
 
 require('paq-nvim').install()
 require('paq-nvim').clean()
 
-vim.g['completeopt']="preview"
-
-local on_attach = function(client, bufnr)
-    require('completion').on_attach()
-
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
-    -- Set some keybinds conditional on server capabilities
-    if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    end
-  end
-
-  local servers = {'pylsp'}
-  for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-      on_attach = on_attach,
-    }
-  end
 
 -- Shortcur binding section begin --
+vim.api.nvim_set_keymap( 'n',   '<F9>', ':TagbarToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap( 'n',   '<F10>', ':TagbarToggle<CR>', {noremap = true})
 vim.api.nvim_set_keymap( 'n',   '<F11>', ':NERDTreeToggle<CR>', {noremap = true})
 vim.api.nvim_set_keymap( 'n',   '<F12>', ':Telescope find_files<CR>', {noremap = true})
 -- Shortcur binding section end --
