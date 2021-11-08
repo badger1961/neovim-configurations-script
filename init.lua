ce531fae925920bdebdbfaff5a681c59e405288c
aag031 <alexander.l.gorlov@gmail.com>
2021-11-08 13:44:44 +0300

    remove obsolete version number

--- init.lua
+++
@@ -1,9 +0,0 @@
--- neo vim configuration programm
--- version A302
--- Version settings.lua A102 --
--- Version plugins.lua  A102 --
--- Version keymaps.lua  A100 --
-require ("plugins")
-require ("settings")
-require ("keymaps")
-
--- neovim/init.lua
+++ neovim/init.lua
@@ -1,8 +1,5 @@
 -- neo vim configuration programm
 -- version A310
--- Version settings.lua A102 --
--- Version plugins.lua  A110 --
--- Version keymaps.lua  A101 --
 require ("plugins")
 require ("settings")
 require ("keymaps")
